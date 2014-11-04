75032538d9b878f52db37d0c313716f4d74b6c49
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-11-04 15:04:48 -0600

    lab 12 100 pint

--- 100pt.py
+++ 100pt.py
@@ -26,19 +26,26 @@ class MyApp:
 		self.button1.pack(side=LEFT)
 		
 	        # Add a second button!
-				
+		self.button2 = Button(self.myContainer1)
+		self.button2.configure(text="Right", background= "blue")
+		self.button2.pack(side=LEFT)	
 						
 		self.button1.bind("<Button-1>", self.button1Click) 
 		drawpad.pack(side=BOTTOM)
 		
-
+		self.button2.bind("<Button-1>", self.button2Click) 
+		drawpad.pack(side=BOTTOM)
 		
 	def button1Click(self, event):   
 		# Make me move to the left!
 		global oval
 		global drawpad
-	
+		drawpad.move(oval,-10,0)
 	# Add the event handler for the second button!
+	def button2Click(self,event):
+	        global oval
+	        global drawpad
+	        drawpad.move(oval,10,0)
 	
 		
 myapp = MyApp(root)
